371b2e774e49f0a2173f81443836ab9ad2121d67
dcordova <dillon.cordova@warnerbros.com>
2020-01-30 15:34:50 -0800

    Full WORKING STATE! added dynamo and able to debug

--- backend/record_song_vote.js
+++ backend/record_song_vote.js
@@ -5,6 +5,7 @@ const ing       = require('../lib/ing');
 const drilldown = require('../lib/drilldown');
 
 module.exports.handler = async (event) => {
+  
   const service   = services();
   const song_name = event.body.songName;
   const result    = {
@@ -26,7 +27,7 @@ module.exports.handler = async (event) => {
   };
 
   const [err, data] = await ing( service.dynamo.updateItem(params).promise() );
-  
+
   if( err || !drilldown(data, 'Attributes.votes'.split('.')) ){
     result.status = 404;
     result.body   = JSON.stringify(err || '[data.Attributes.votes] was not found');
